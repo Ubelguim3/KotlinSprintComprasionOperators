0dfb59293c7d7637809134766f0acd66784033ed
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-24 23:25:04 +0300

    Task 3 completed

---
+++ src/main/kotlin/lesson4/lesson4_Task3.kt
@@ -0,0 +1,8 @@
+fun task3() {
+    val weatherToday = true
+    val tentStatus = true
+    val humidity = 20
+    val timeOfYear = "Зима"
+    println("Благоприятны ли сейчас условия для роста бобовых: " +
+            "${weatherToday && tentStatus &&  humidity == 20 && timeOfYear != "Зима"}")
+}
