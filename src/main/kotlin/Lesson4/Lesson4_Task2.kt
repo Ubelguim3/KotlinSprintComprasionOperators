6f258e5c3c1b88a980923c2a70ca096e534bc6af
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-24 22:43:25 +0300

    Task 2 completed

---
+++ src/main/kotlin/Lesson4/Lesson4_Task2.kt
@@ -0,0 +1,10 @@
+package Lesson4
+
+fun task2() {
+    var weight = 20
+    var capacity = 80
+    println("Груз с весом $weight кг и объемом $capacity л соответствует категории 'Average': ${((weight > 35) and (weight <= 100))  && (capacity < 100)}")
+    weight = 50
+    capacity = 100
+    println("Груз с весом $weight кг и объемом $capacity л соответствует категории 'Average': ${((weight > 35) and (weight <= 100))  && (capacity < 100)}")
+}
