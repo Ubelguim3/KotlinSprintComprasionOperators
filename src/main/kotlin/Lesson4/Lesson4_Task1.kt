3dc95460ac18b5a4a7d1ef2a932fe9e75c874435
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-24 22:21:50 +0300

    Task 1 completed

---
+++ src/main/kotlin/Lesson4/Lesson4_Task1.kt
@@ -0,0 +1,5 @@
+fun task1() {
+    val tablesBookedToday = 13
+    val tablesBookedTomorrow = 9
+    println("Доступность столиков на сегодня: ${tablesBookedToday < numberOfTables}\nДоступность столиков на сегодня: ${tablesBookedTomorrow < numberOfTables}")
+}
