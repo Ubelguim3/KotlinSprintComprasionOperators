8738b92da82496b80d48943218d700a35d5ed44a
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-28 02:15:51 +0300

    KS-4-1 Task completed

--- src/main/kotlin/Lesson4/Lesson4_Task1.kt
+++ src/main/kotlin/Lesson4/Lesson4_Task1.kt
@@ -3,8 +3,8 @@ const val NUMBER_OF_TABLES = 13
 fun task1() {
     val tablesBookedToday = 13
     val tablesBookedTomorrow = 9
-    println("""Доступность столиков на сегодня: ${tablesBookedToday < NUMBER_OF_TABLES} 
-Доступность столиков на сегодня: ${tablesBookedTomorrow < NUMBER_OF_TABLES}""".trimMargin())
+    println("Доступность столиков на сегодня: ${tablesBookedToday < NUMBER_OF_TABLES}\n" +
+          "Доступность столиков на сегодня: ${tablesBookedTomorrow < NUMBER_OF_TABLES}")
 }
 
 fun main() {
